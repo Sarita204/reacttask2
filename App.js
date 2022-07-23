cce15f27bd44fbe427bf0fe0987500c6ca179173
sarita <aritasp204@gmail.com>
2022-07-23 15:37:52 +0530

    jsx craeting

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Greet from './components/greet'
-import Welcome from './components/New folder/Welcome'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greet />
+        <hr></hr>
         <Welcome />
+        <hr></hr>
+        <Hello />
       </div>
     );
   }
---
+++ Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+ const Hello = () => {
+    return (
+        <div>
+            <h1>JSX Version component..!</h1>
+        </div>
+    )
+ }
+
+ export default Hello;
